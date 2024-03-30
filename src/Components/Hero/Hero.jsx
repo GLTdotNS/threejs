92623a12c4bc572421e1029275ab9746c6ed7d41
GLTdotNS <georgitonkov94@gmail.com>
2024-03-30 11:24:52 +0100

    add files

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -100,7 +100,7 @@ const Hero = () => {
                 <div className="mt-3 sm:mt-0 sm:ml-3 mb-36">
                   <a
                     download
-                    href="./Georgi-Tonkov.pdf"
+                    href="./Georgi Tonkov.pdf"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-gray-800 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                   >
                     Download CV
